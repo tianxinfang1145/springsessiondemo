b116aecd28720d9b33e638b6bdf2a20157866732
github1145 <74097304+tianxinfang1145@users.noreply.github.com>
2020-11-09 16:25:18 +0800

    Update TestController.java

--- src/main/java/com/offcn/springsessiondemo/controller/TestController.java
+++ src/main/java/com/offcn/springsessiondemo/controller/TestController.java
@@ -15,6 +15,7 @@ public class TestController {
 
     @GetMapping("/get")
     public String getSession(HttpSession session){
+        shdhdhhsshkshsjkj
         return (String)session.getAttribute("msg");
     }
 }
