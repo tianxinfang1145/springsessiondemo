e03a3917a4105c48e75b48a14fa7fcc515666ed6
github1145 <74097304+tianxinfang1145@users.noreply.github.com>
2020-11-09 16:33:50 +0800

    Update TestController.java

--- src/main/java/com/offcn/springsessiondemo/controller/TestController.java
+++ src/main/java/com/offcn/springsessiondemo/controller/TestController.java
@@ -15,9 +15,6 @@ public class TestController {
 
     @GetMapping("/get")
     public String getSession(HttpSession session){
-        System.out.println("github123");
-         System.out.println("你好呀");
-        System.out.println("我很好呀");
         return (String)session.getAttribute("msg");
     }
 }
